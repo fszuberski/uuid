7dae238fbd33c01c4936aa20515a99be8b09f2e1
Filip Szuberski <filip.szuberski@gmail.com>
2021-04-15 22:04:16 +0100

    feat: print multiple uuids

--- uuid.go
+++ uuid.go
@@ -2,10 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
 	"github.com/google/uuid"
 )
 
 func main() {
-	fmt.Println(uuid.New())
+	num := parseArgs()
+	for i := 0; i < num; i++ {
+		fmt.Println(uuid.New())
+	}
+}
+
+func parseArgs() int {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		return 1
+	}
+
+	num, err := strconv.Atoi(args[0])
+	if err != nil {
+		log.Fatalf("invalid argument: %s", args[0])
+	}
+
+	if num < 0 {
+		return 1
+	}
+
+	return num
 }
